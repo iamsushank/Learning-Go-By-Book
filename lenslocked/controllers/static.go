43ff366b2e46d42c9a9b1c225da8fd25120cc68f
Sushank Mandal <45608530+iamsushank@users.noreply.github.com>
2023-03-07 09:06:08 +0530

    config docker

--- lenslocked/controllers/static.go
+++ lenslocked/controllers/static.go
@@ -2,11 +2,12 @@ package controllers
 
 import (
 	"github.com/iamsushank/lenslocked/views"
+	"html/template"
 	"net/http"
 )
 
 type Static struct {
-	Template views.Template
+	Template Template
 }
 
 func (static Static) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -16,5 +17,28 @@ func (static Static) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 func StaticHandler(tpl views.Template) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		tpl.Execute(w, nil)
+	}
+}
+
+func FAQ(tpl views.Template) http.HandlerFunc {
+	questions := []struct {
+		Question string
+		Answer   template.HTML
+	}{
+		{
+			Question: "Is there a free version?",
+			Answer:   "Yes! We offer a free trial for 30 days on any paid plans.",
+		},
+		{
+			Question: "What are your support hours?",
+			Answer:   "We have support staff answering emails 24/7, though response ",
+		},
+		{
+			Question: "How do I contact support?",
+			Answer:   `Email us - <a href="mailto:mandal678@gmail.com">mandal678@gmail.com`,
+		},
+	}
+	return func(w http.ResponseWriter, r *http.Request) {
+		tpl.Execute(w, questions)
 	}
 }
